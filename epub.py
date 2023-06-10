be2b798993320236a377749ea420026bfb72da47
AcV134 <anirudh.c.vinayan@gmail.com>
2023-06-10 23:18:35 +0530

    updated epub.py

--- epub.py
+++ epub.py
@@ -14,32 +14,30 @@ class Window(QMainWindow):
         self.layout = QVBoxLayout()
 
         book = epub.read_epub('Spellslinger_-_Sebastien_de_Castell.epub')
-        self.text_list = []
+        self.all_list = []
         self.image_text = QtWidgets.QVBoxLayout()
 
         for item in book.get_items():
             if item.get_type() == ebooklib.ITEM_IMAGE:
-                label = QLabel(self)
                 pixmap = QPixmap()
                 pixmap.loadFromData(item.get_content())
-                label.setPixmap(pixmap)
-                self.image_text.addWidget(label)
+                self.all_list.append(pixmap)
 
             elif item.get_type() == ebooklib.ITEM_DOCUMENT:
-                text = item.get_content().decode("utf-8")
-                self.text_list.append(text)
+                item = item.get_content().decode("utf-8")
+                self.all_list.append(item)
         self.current_index = 0
         self.counter(0)
 
     def counter(self,value):
         if value == 0 :
-            self.create_word(self.text_list[self.current_index])
+            self.create_wid(self.all_list[self.current_index])
         elif value==1 and self.current_index!=0:
             self.current_index = self.current_index - 1
-            self.create_word(self.text_list[self.current_index])
-        elif value==2 and self.current_index!=(len(self.text_list)-1):
+            self.create_wid(self.all_list[self.current_index])
+        elif value==2 and self.current_index!=(len(self.all_list)-1):
             self.current_index = self.current_index + 1
-            self.create_word(self.text_list[self.current_index])
+            self.create_wid(self.all_list[self.current_index])
 
     def scroller(self, to_print):
         self.epub_page = QWidget()
@@ -49,23 +47,28 @@ class Window(QMainWindow):
         scroll_area.setWidget(self.epub_page)
         self.setCentralWidget(scroll_area)
 
-    def create_word(self, text):
-        self.text_print = QVBoxLayout()
-        text_layout = QLabel(self)
-        text_layout.setWordWrap(True)
-        text_layout.setText(text)
-        self.text_print.addWidget(text_layout)
+    def create_wid(self, item):
+        self.wid_print = QVBoxLayout()
 
-        # Add the button as a widget in the text_print layout
+        if isinstance(item, QPixmap):
+            label = QLabel(self)
+            label.setPixmap(item)
+            self.wid_print.addWidget(label)
+        elif isinstance(item, str):
+            text_layout = QLabel(self)
+            text_layout.setWordWrap(True)
+            text_layout.setText(item)
+            self.wid_print.addWidget(text_layout)
+
         button_forward = QPushButton('->', self)
         button_forward.clicked.connect(self.button_forward_clicked)
-        self.text_print.addWidget(button_forward)
+        self.wid_print.addWidget(button_forward)
 
         button_backward = QPushButton('<-', self)
         button_backward.clicked.connect(self.button_backward_clicked)
-        self.text_print.addWidget(button_backward)
+        self.wid_print.addWidget(button_backward)
 
-        self.scroller(self.text_print)
+        self.scroller(self.wid_print)
 
     def button_forward_clicked(self):
         self.counter(2)
