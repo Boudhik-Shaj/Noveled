b8f47c37f4ec3b663b2803294d5a8ba788fab268
AcV134 <anirudh.c.vinayan@gmail.com>
2023-06-11 02:05:32 +0530

    updated epub.py

--- epub.py
+++ epub.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QVBoxLayout, QPushButton, QScrollArea, QWidget
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QScrollArea, QWidget, QShortcut
+from PyQt5.QtGui import QPixmap, QFont, QKeySequence
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QTimer, QEvent, pyqtSignal
+
 import ebooklib
 from ebooklib import epub
 import sys
@@ -9,23 +11,29 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Noveled")
-        self.setGeometry(0, 0, 1920, 1080)
+        # self.setGeometry(0, 0, 1920, 1080)
+        self.setGeometry(100, 100, 800, 600)
+        self.setStyleSheet("background-color: #101117;")
+
+        self.font = QFont()
+        self.font.setFamily("Arial")
+        self.font.setPointSize(12)
 
         self.layout = QVBoxLayout()
 
         book = epub.read_epub('Spellslinger_-_Sebastien_de_Castell.epub')
         self.all_list = []
-        self.image_text = QtWidgets.QVBoxLayout()
 
         for item in book.get_items():
-            if item.get_type() == ebooklib.ITEM_IMAGE:
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                item = item.get_content().decode("utf-8")
+                self.all_list.append(item)
+            elif item.get_type() == ebooklib.ITEM_IMAGE:
                 pixmap = QPixmap()
                 pixmap.loadFromData(item.get_content())
                 self.all_list.append(pixmap)
 
-            elif item.get_type() == ebooklib.ITEM_DOCUMENT:
-                item = item.get_content().decode("utf-8")
-                self.all_list.append(item)
+            
         self.current_index = 0
         self.counter(0)
 
@@ -58,22 +66,47 @@ class Window(QMainWindow):
             text_layout = QLabel(self)
             text_layout.setWordWrap(True)
             text_layout.setText(item)
+            text_layout.setFont(self.font)
+            text_layout.setStyleSheet("color: white;")
             self.wid_print.addWidget(text_layout)
+        
+        button_layout = QHBoxLayout()
 
         button_forward = QPushButton('->', self)
+        button_forward.setStyleSheet("color: white;")
+        button_forward.setStyleSheet("background-color: grey;")
+
+        QShortcut(QKeySequence('Right'), self).activated.connect(self.button_forward_clicked)
         button_forward.clicked.connect(self.button_forward_clicked)
-        self.wid_print.addWidget(button_forward)
+
+        
+        button_random = QPushButton('  ', self)
+        button_random.setStyleSheet("color: white;")
+        button_random.setFixedSize(1400, 30)
+        button_random.setStyleSheet("background-color: #101117;")
+        button_random.clicked.connect(button_random.click)
 
         button_backward = QPushButton('<-', self)
+        button_backward.setStyleSheet("color: white;")
+        button_backward.setStyleSheet("background-color: grey;")
+
+        QShortcut(QKeySequence('Left'), self).activated.connect(self.button_backward_clicked)
         button_backward.clicked.connect(self.button_backward_clicked)
-        self.wid_print.addWidget(button_backward)
+
+        button_layout.addWidget(button_backward)
+        button_layout.addWidget(button_random)
+        button_layout.addWidget(button_forward)
+
+        self.wid_print.addLayout(button_layout)
 
         self.scroller(self.wid_print)
 
     def button_forward_clicked(self):
+        print("Button Forward Clicked")
         self.counter(2)
 
     def button_backward_clicked(self):
+        print("Button Backwards Clicked")
         self.counter(1)
 
 if __name__ == "__main__":
