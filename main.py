e38988481646fa75c7787007799203f2f873fae0
Boudhik Shaj <82608513+Boudhik-Shaj@users.noreply.github.com>
2023-06-10 13:42:14 +0530

    main created

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
+
+
+class window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Noveled")
+        self.setGeometry(100, 100, 300, 300)
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = window()
+    win.show()
+    sys.exit(app.exec_())
